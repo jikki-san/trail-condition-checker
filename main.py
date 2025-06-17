8d4dbd92d4e132749f404ed5a0e5877cd41f0156
Jackson Eckert <jaxon.eckert@gmail.com>
2025-06-17 13:23:35 -0500

    Add notifications

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+from email.mime.text import MIMEText
+import os
 import requests
+import smtplib
 
 
-def find_trail(trail_name):
-    def match_trail_name(tag):
-        return tag.name == "tr" and tag.contents[0].text == trail_name
-    return match_trail_name
+def send_notification(notification_msg: str):
+    notification_phone = os.environ.get("NOTIFICATION_PHONE_NUMBER")
+    from_email = os.environ.get("FROM_EMAIL")
+    app_pw = os.environ.get("FROM_EMAIL_APP_PW")
+
+    to_number = f"{notification_phone}@vtext.com"
+
+    # Create the message
+    msg = MIMEText(notification_msg)
+    msg["From"] = from_email
+    msg["To"] = to_number
+    msg["Subject"] = ""  # SMS doesn't need a subject
+
+    # Send the email (SMS)
+    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+        server.starttls()
+        server.login(from_email, app_pw)
+        server.sendmail(from_email, to_number, msg.as_string())
+
+    print("Message sent!")
 
 
 def main():
+    load_dotenv()
     trail_name = "Skyline Trail"
     res = requests.get(
         "https://www.nps.gov/mora/planyourvisit/trails-and-backcountry-camp-conditions.htm")
@@ -20,7 +41,8 @@ def main():
     trail_conditions = row.text
     row = row.next_sibling.next_sibling
     updated_at = row.text
-    print(f"{trail_name}: {snow_cover_pct}% snow cover, {trail_conditions}. Last updated {updated_at}.")
+    status_string = f"{trail_name}: {snow_cover_pct}% snow cover, {trail_conditions}. Last updated {updated_at}."
+    send_notification(status_string)
 
 
 if __name__ == "__main__":
